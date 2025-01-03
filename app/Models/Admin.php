9a3436823f9051294881c232f5565a6d71027703
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-03 18:25:27 +0900

    管理者用のコントローラーの作成

---
+++ app/Models/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable as NotificationsNotifiable;
+use Illunminate\Notifications\Notifiable;
+use Laravel\sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
