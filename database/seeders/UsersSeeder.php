0f60b87123ee975f3cd26f3d9413069af8e2ed18
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-06 01:24:05 +0900

    ユーザーデータベースにシーディング完了

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
@@ -12,6 +14,20 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'name' => '侍',
+            'kana' => 'さむらい',
+            'email' => 'samurai@example,com',
+            'email_verified_at' => '2025-01-01 00:00:00',
+            'password' => Hash::make('password'),
+            'postal_code' => '6500012',
+            'address' => '兵庫県神戸市中央区相生町',
+            'phone_number' => '09022223333',
+            'birthday' => '',
+            'occupation' => '',
+            'remember_token' => '',
+            'created_at' => '2024-12-12 11:11:11',
+            'updated_at' => '2024-12-22 11:11:11'
+        ]);
     }
 }
