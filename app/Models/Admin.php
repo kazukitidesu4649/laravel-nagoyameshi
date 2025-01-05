defb0388e38eeae3a6aef0b3aa2eaa80e980507a
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-05 10:57:50 +0900

    コンフリクトの修正

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,9 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable as NotificationsNotifiable;
-use Illunminate\Notifications\Notifiable;
-use Laravel\sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
--- routes/auth.php
+++ routes/auth.php
@@ -12,7 +12,8 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\Admin;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest', 'guest:admin')->group(function () {
+
+Route::middleware(['guest', 'guest:admin'])->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
@@ -37,7 +38,7 @@ Route::middleware('guest', 'guest:admin')->group(function () {
 });
 
 Route::middleware('guest:admin')->group(function(){
-    Route::get('admin/login', [Admin\Auth\AuthenticatadSessionController::class, 'create'])
+    Route::get('admin/login', [Admin\Auth\AuthenticatedSessionController::class, 'create'])
                 ->name('admin.login');
     
     Route::post('admin/login', [Admin\Auth\AuthenticatedSessionController::class, 'store']);
