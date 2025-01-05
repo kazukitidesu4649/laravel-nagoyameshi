1b0580cf3327486ac4324a71f5d04033fd17d123
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-06 02:16:03 +0900

    管理者側のUserControllerを編集

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request) {
+        //検索ワードを取得
+        $keyword = $request->input('keyword');
+        
+        // クエリの作成
+        $query = User::query()->when($keyword, function ($query, $keyword){
+                $query->where('name', 'LIKE', "%{$keyword}%")
+                        ->orwhere('kana', 'LIKE',"%{$keyword}%");
+            });
+        // ページネーションでデータを取得
+        $users = $query->paginate(15);
+        //総数の計算
+        $total = $query->count();
+        
+        //ビューにデータを渡す
+        return view('users.index', compact('users', 'keyword', 'total'));    
+    }
+
+    public function show(User $user) {
+        return view('Users.show', compact('user'));
+    }
 }
