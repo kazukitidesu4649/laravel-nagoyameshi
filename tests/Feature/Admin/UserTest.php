f37cb5537ed3d9886e23643bcb7127a672f18954
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-06 17:38:01 +0900

    起きていたAdmin/UserTestのエラーを回収し、テスト成功

--- tests/Feature/Admin/UserTest.php
+++ tests/Feature/Admin/UserTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use Database\Factories\UserFactory;
+use Database\Factories\AdminFactory;
 
 class UserTest extends TestCase
 {
@@ -23,9 +25,9 @@ class UserTest extends TestCase
     // 未ログインユーザーが会員一覧ページにアクセスできないテスト
     public function test_guest_cannot_access_index()
     {
-        $response = $this->get(route('users.index'));
+        $response = $this->get(route('admin.users.index'));
 
-        $response->assertRedirect(route('login'));
+        $response->assertRedirect(route('admin.login'));
     }
 
     // ログイン済みの一般ユーザーは管理者側の会員一覧ページにアクセスできない
@@ -33,18 +35,48 @@ class UserTest extends TestCase
     {
         $user = User::factory()->create();
 
-        $response = $this->actingAs($user)->get(route('users.index', '$user'));
+        $response = $this->actingAs($user)->get(route('admin.users.index', '$user'));
 
-        $response->assertStatus(403);
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    // ログイン済みの管理者は管理者側の会員一覧ページにアクセスできる
+    public function test_admin_cannot_access_index()
+    {
+        $admin = AdminFactory::new()->create();
+
+        $response = $this->actingAs($admin, 'admin')->get(route('admin.users.index', '$admin'));
+
+        $response->assertStatus(200);
     }
 
     // 未ログインのユーザーは管理者側の会員詳細ページにアクセスできない
     public function test_guest_cannot_access_show()
     {
+        $response = $this->get(route('admin.users.show', '$user'));
+
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    // ログイン済みの一般ユーザーは管理者側の会員詳細ページにアクセスできない
+    public function test_user_cannot_access_show()
+    {
         $user = User::factory()->create();
 
-        $response = $this->get(route('users.show', '$user'));
+        $response = $this->actingAs($user)->get(route('admin.users.show', '$user'));
 
-        $response->assertRedirect(route('login'));
+        $response->assertRedirect(route('admin.login'));
+    }
+
+    // ログイン済みの管理者は管理者側の会員詳細ページにアクセスできる
+    public function test_admin_cannot_access_show()
+    {
+        $admin = AdminFactory::new()->create();
+
+        $user = UserFactory::new()->create();
+
+        $response = $this->actingAs($admin, 'admin')->get(route('admin.users.show', ['user' => $user->id]));
+
+        $response->assertStatus(200);
     }
 }
