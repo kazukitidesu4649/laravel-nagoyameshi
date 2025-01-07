9ff3fb6513ad378c5b987399f988876a13bb6068
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-07 11:04:37 +0900

    管理者用店舗コントローラーに一覧機能を追加

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
 class RestaurantController extends Controller
 {
-    //
+    // 店舗一覧ページ
+    public function index(Request $request){
+
+        $keyword = $request->input('keyword');
+        
+        $query = Restaurant::query()
+                             ->when($keyword, function($query,$keyword) {
+                                return $query->where('name', 'LIKE', "%{$keyword}%");
+                             });
+
+        $total = $query->count();
+
+        return view('admin.restaurants.index', compact('restaurants', 'keyword', 'total'));
+    }
 }
