a09e0c9a69c1752ccd020127ddbc30c973c53d15
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-08 00:37:43 +0900

    店舗用のseederを作成

---
+++ database/seeders/RestaurantSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RestaurantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('restaurants')->insert([
+            'name' => 'テスト',
+            'description' => 'テスト',
+            'lowest_price' => '1000',
+            'highest_price' => '5000',
+            'postal_code' => '0000000',
+            'address' => 'テスト',
+            'opening_time' => '10:00:00',
+            'closing_time' => '20:00:00',
+            'seating_capacity' => '50'
+        ]);
+    }
+}
