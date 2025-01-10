c33c4a9971bfa698275418c3f16f287694c4490f
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-10 18:00:56 +0900

    CategoryControllerをAdminディレクトリに移動

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+
+class CategoryController extends Controller
+{
+    // indexアクション（カテゴリ一覧ページ）
+    public function index (Request $request) {
+        
+    }
+}
