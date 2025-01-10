504270489bf970549e442f38683a20cb98e91b5a
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-10 18:14:05 +0900

    カテゴリコントローラにindexアクションを追加

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -11,6 +11,16 @@ class CategoryController extends Controller
 {
     // indexアクション（カテゴリ一覧ページ）
     public function index (Request $request) {
-        
+        $keyword = $request->input('keyword');
+
+        if($keyword) {
+            $categories = Category::where('id', 'name', "%{$keyword}%");
+        } else {
+            $categories = Category::paginate(15);
+        }
+
+        $total = $categories->total();
+
+        return view('admin.categories.index', compact('categories', 'keyword', 'total'));
     }
 }
