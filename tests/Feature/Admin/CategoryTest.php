3e198b38fccc37b27cca7e25e3b7aa1651290d15
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-12 00:49:49 +0900

    storeアクションのテスト完了

--- tests/Feature/Admin/CategoryTest.php
+++ tests/Feature/Admin/CategoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Admin;
 
 use Database\Factories\UserFactory;
 use Database\Factories\AdminFactory;
+use Database\Factories\CategoryFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -35,5 +36,30 @@ class CategoryTest extends TestCase
         $response->assertStatus(200);
     }
 
+    // storeアクション（カテゴリ登録機能）
+    public function test_guest_cannot_store_categories()
+    {
+        $category = CategoryFactory::new()->create();
 
+        $response = $this->post('/admin/categories',[$category]);
+        $response->assertRedirect('/admin/login');
+    }
+
+    public function test_user_cannot_store_categories()
+    {   
+        $user = UserFactory::new()->create();
+        $category = CategoryFactory::new()->create();
+
+        $response = $this->actingAs($user)->post('/admin/categories',[$category]);
+        $response->assertRedirect('/admin/login');
+    }
+
+    public function test_admin_can_store_categories()
+    {   
+        $admin = AdminFactory::new()->create();
+        $category = CategoryFactory::new()->create();
+
+        $response = $this->actingAs($admin, 'admin')->post('/admin/categories',[$category]);
+        $response->assertStatus(302);
+    }
 }
