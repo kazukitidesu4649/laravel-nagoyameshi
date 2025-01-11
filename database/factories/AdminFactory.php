2ea084cea0d739db77fb486403e8d26d507e52b0
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-12 00:31:03 +0900

    indexアクションのテスト完了

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Admin;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,8 +25,8 @@ class AdminFactory extends Factory
     public function definition(): array
     {
         return [
-            'email' => fake()->unique()->safeEmail(),
-            'password' => static::$password ??= Hash::make('password'),
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10)
         ];
     }
--- tests/Feature/Admin/CategoryTest.php
+++ tests/Feature/Admin/CategoryTest.php
@@ -2,19 +2,38 @@
 
 namespace Tests\Feature\Admin;
 
+use Database\Factories\UserFactory;
+use Database\Factories\AdminFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    // indexアクション（カテゴリ一覧ページ）
+    public function test_guest_cannot_admin_categories_index()
+    {
+        $response = $this->get('/admin/categories');
+        $response->assertRedirect('/admin/login');
+    }
+
+    public function test_user_cannot_admin_categories_index()
+    {
+        $user = UserFactory::new()->create();
+
+        $response = $this->actingAs($user)->get('/admin/categories');
+        $response->assertRedirect('/admin/login');
+    }
+
+    public function test_admin_cannot_admin_categories_index()
+    {
+        $admin = AdminFactory::new()->create();
+
+        $response = $this->actingAs($admin, 'admin')->get('/admin/categories');
         $response->assertStatus(200);
     }
+
+
 }
