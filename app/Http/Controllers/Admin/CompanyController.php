d349a4af2f9c2532fdec227f4d75a0503a8de291
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-15 01:40:31 +0900

    会社概要用のコントローラーにindex, edit, updateアクションを実装

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -3,9 +3,54 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Company;
 use Illuminate\Http\Request;
+
 
 class CompanyController extends Controller
 {
-    //
+    // indexアクション（会社概要ページ）
+
+    public function index() {
+
+        $company = Company::first();
+
+        return view('admin.company.index', compact('company'));
+    }
+
+    // editアクション（会社概要編集ページ）
+
+    public function edit(Company $company) {
+
+        return view('admin.company.edit', compact('company'));
+    }
+
+    // updateアクション（会社概要更新機能）
+
+    public function update(Request $request, Company $company) {
+
+        $request->validate([
+            'name' => 'required',
+            'postal_code' => 'required|numeric|digits:7',
+            'address' => 'required',
+            'representative' => 'required',
+            'establishment_date' => 'required',
+            'capital' => 'required',
+            'business' => 'required',
+            'number_of_employees' => 'required',
+        ]);
+
+        $company->name = $request->name;
+        $company->postal_code = $request->postal_code;
+        $company->address = $request->address;
+        $company->representative = $request->representative;
+        $company->establishment_date = $request->establishment_date;
+        $company->capital = $request->capital;
+        $company->business = $request->business;
+        $company->number_of_employees = $request->number_of_employees;
+        $company->save();
+
+        return redirect()->route('admin.company.index')
+                        ->with('flash_message', '会社概要を編集しました。');
+    }
 }
