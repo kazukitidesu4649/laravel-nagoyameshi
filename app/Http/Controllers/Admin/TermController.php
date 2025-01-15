44eac5a4153ebbf3c0befff56ccf3d455c4f4f75
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-15 09:10:58 +0900

    利用規約用のindex, edit, updateを編集

---
+++ app/Http/Controllers/Admin/TermController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Term;
+use Illuminate\Http\Request;
+
+class TermController extends Controller
+{
+    // indexアクション（利用規約ページ）
+    public function index() {
+        
+        $term = Term::first();
+
+        return view('admin/terms/index', compact('term'));
+    }
+
+    // editアクション（利用規約編集ページ）
+    public function edit(Term $term) {
+
+        return view('admin/terms/edit', compact('term'));
+    }
+
+    // updateアクション（利用規約更新機能）
+    public function update(Request $request, Term $term) {
+
+        $request->validate(['content' => 'required']);
+        $term->content = $request->content;
+        $term->save();
+
+        return redirect('admin/terms/index')
+                ->with('flash_message', '利用規約を編集しました。');
+    }
+}
