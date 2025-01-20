438ac5dcc2e6ad0fb8eca8fe1eb1760df83304a2
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-20 10:18:11 +0900

    storeアクションを実装

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -14,4 +14,21 @@ class SubscriptionController extends Controller
         return view('subscription.create', compact('intent'));
     }
 
+    // storeアクション（登録機能）
+    public function store(Request $request) {
+
+        $user = Auth::user();
+
+        $paymentMethod = $request->input('payment_method');
+
+        try {
+            $user->newSubscription('premium_plan', 'price_1Qj9UuLrDOeQcDxNv4X1he93')
+            ->create($paymentMethod);
+
+            session()->flash('flash_message', '有料プランへの登録が完了しました。');
+            return redirect()->route('home');
+        } catch (\Exception $se) {
+            return back()->withErrors(['error' => '登録処理中にエラーが発生しました:'. $e->getMessage()]);
+        }
+    }
 }
