c3f9ec01544ac8b41b339765d2b9fd00081cbd4d
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-23 12:45:35 +0900

    ReviewControllerを作成

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Models\Review;
+
+class ReviewController extends Controller
+{
+    // indexアクション（レビュー一覧ページ）
+    public function 
+}
