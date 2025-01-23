728b400a27a57b08965c96c5d4f72177b2283ca0
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-23 12:10:15 +0900

    reviewsテーブルを作成

--- database/migrations/2025_01_22_112530_create_reviews_table.php
+++ database/migrations/2025_01_22_112530_create_reviews_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            $table->text('content');
+            $table->unsignedInteger('score');
+            $table->foreignId('restaurant_id')
+                    ->constrained()
+                    ->cascadeOnDelete();
+            $table->foreignId('user_id')
+                    ->constrained()
+                    ->cascadeOnUpdate();
             $table->timestamps();
         });
     }
