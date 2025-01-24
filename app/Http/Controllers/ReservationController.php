19f51f79c3511f6567fb80af284238570b7a56e0
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-24 17:49:38 +0900

    予約コントローラーに index, create, store, destroyアクションの定義

---
+++ app/Http/Controllers/ReservationController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reservation;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class ReservationController extends Controller
+{
+    // indexアクション（予約一覧ページ）
+    public function index(Reservation $reservation){
+        
+        $reservation = Reservation::where('user_id', auth()->id())
+            ->orderBy('reserved_datetime', 'desc')
+            ->paginate(15);
+        
+            return view('reservations.index', compact('reservations'));
+    }
+
+    public function create(Restaurant $restaurant) {
+
+        return view('reservations.create', compact('restaurant'));
+    }
+
+    public function store(Request $request, Restaurant $restaurant) {
+
+        $validated = $request->validate([
+            'reservation_date' => 'required|date_format:Y-m-d',
+            'reservation_time' => 'required|date_format:H-i',
+            'number_of_people' => 'required|integer|between:1,50',
+        ]);
+
+        $reserved_datetime = $validated['reservation_date'] . ' ' . $validated['reservation_time'];
+
+        $reservation = new Reservation([
+            'reserved_datetime' => $reserved_datetime,
+            'number_of_people' => $validated['number_of_people'],
+            'restaurant_id' => $restaurant->id,
+            'user_id' => auth()->id(),
+        ]);
+
+        $reservation->save();
+
+        return redirect()->route('reservations.index')
+            ->with('with_message', '予約が完了しました。');
+    }
+
+    public function destroy(Reservation $reservation) {
+
+        if ($reservation->user_id !== auth()->id()) {
+            return redirect()->route('reservations.index')
+                ->with('flash_message', '予約をキャンセルしました。');
+        }
+
+        $reservation->delete();
+
+        return redirect()->route('reservations.index')
+            ->with('with_message', '予約をキャンセルしました。');
+    }
+}
