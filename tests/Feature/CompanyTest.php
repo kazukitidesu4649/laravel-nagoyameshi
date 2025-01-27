c18b276f66f4fde92cbf6dd9bf1b091e464ec71b
kazukitidesu4649 <kazukitidesu@gmail.com>
2025-01-27 18:35:33 +0900

    ユニットテスト終了

--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -2,19 +2,42 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\Admin;
+use App\Models\Company;
+use Database\Factories\CompanyFactory;
+use Database\Factories\UserFactory;
+use Database\Factories\AdminFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CompanyTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    public function test_guest_can_access_company() {
+        $company = CompanyFactory::new()->create();
+
+        $response = $this->get(route('company.index'));
         $response->assertStatus(200);
+        $response->assertViewIs('company.index');
+    }
+
+    public function test_user_can_access_company() {
+        $company = CompanyFactory::new()->create();
+        $user = UserFactory::new()->create();
+
+        $response = $this->actingAs($user)->get(route('company.index'));
+        $response->assertStatus(200);
+        $response->assertViewIs('company.index');
+    }
+
+    public function test_admin_cannot_access_company() {
+        $company = CompanyFactory::new()->create();
+        $admin = AdminFactory::new()->create();
+
+        $response = $this->actingAs($admin, 'admin')->get(route('company.index'));
+        $response->assertRedirect(route('admin.home'));
     }
 }
--- tests/Feature/TermTest.php
+++ tests/Feature/TermTest.php
@@ -2,19 +2,42 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
+use App\Models\Admin;
+use App\Models\Term;
+use Database\Factories\TermFactory;
+use Database\Factories\UserFactory;
+use Database\Factories\AdminFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TermTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
+    public function test_guest_can_access_term() {
+        $term = TermFactory::new()->create();
+
+        $response = $this->get(route('terms.index'));
         $response->assertStatus(200);
+        $response->assertViewIs('terms.index');
+    }
+
+    public function test_user_can_access_term() {
+        $term = TermFactory::new()->create();
+        $user = UserFactory::new()->create();
+
+        $response = $this->actingAs($user)->get(route('terms.index'));
+        $response->assertStatus(200);
+        $response->assertViewIs('terms.index');
+    }
+
+    public function test_admin_cannot_access_term() {
+        $term = TermFactory::new()->create();
+        $admin = AdminFactory::new()->create();
+
+        $response = $this->actingAs($admin, 'admin')->get(route('terms.index'));
+        $response->assertRedirect(route('admin.home'));
     }
 }
